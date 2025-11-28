50ba170f2fc355961e374a4b04268dc6d72d4ac0
abdelrhman-arfat <abdoarfat2006@gmail.com>
2025-11-28 21:29:16 +0200

    Add Comment in Register func to  declaer the section fo dep injection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        // Database:
         $this->app->bind(\App\Repositories\Interfaces\ProductInterface::class, \App\Repositories\Services\ProductService::class);
         $this->app->bind(\App\Repositories\Interfaces\HoldInterface::class, \App\Repositories\Services\HoldService::class);
         $this->app->bind(\App\Repositories\Interfaces\OrderInterface::class, \App\Repositories\Services\OrderService::class);
+        $this->app->bind(\App\Repositories\Interfaces\UserInterface::class, \App\Repositories\Services\UserService::class);
+
+        // Domains:
         $this->app->bind(\App\Domain\Interfaces\RedisInterface::class, \App\Domain\Services\RedisService::class);
     }
 
